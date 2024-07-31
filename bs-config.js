3e2a8db82a51628573f9736b3d05c13b589d8be7
YJPL <26725821+YJPL@users.noreply.github.com>
2024-07-31 14:12:59 +0300

    Delete bs-config.js
    no longer used

--- bs-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  proxy: '127.0.0.1:4000',
-  files: '_site/**/*', // Watch all files in the _site directory
-  server: {
-    baseDir: '_site', // Serve files from the _site directory
-  },
-};
